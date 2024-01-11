66b0a5fce5d0998f508107e310090a5f09145b90
Sofia Bargues <barguessofia@gmail.com>
2024-01-11 23:37:51 +0000

    PARTIAL

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,57 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [status, setStatus] = useState<"initial" | "playing" | "finished">(
+    "initial"
+  );
+  const [timer, setTimer] = useState<number>(0);
+  const [position, setPosition] = useState<[number, number]>([
+    Math.floor(Math.random() * 100),
+    Math.floor(Math.random() * 100),
+  ]);
+  const [score, setScore] = useState<number>(0);
+  function handleClick() {
+    setScore((prevScore) => prevScore + 1);
+    setPosition([
+      Math.floor(Math.random() * 100),
+      Math.floor(Math.random() * 100),
+    ]);
+  }
+  useEffect(() => {
+    let interval: number;
+
+    if (status === "playing") {
+      interval = setInterval(() => setTimer((timer) => timer + 1), 100);
+    }
+
+    return () => clearInterval(interval);
+  }, [status]);
+
   return (
     <main>
       <header>
-        <h1>{0} segundos</h1>
+        <h1>{Math.round((timer / 10) * 100) / 100} segundos</h1>
       </header>
-      <section>
-        <figure />
+      <section style={{ position: "relative", margin: 48, marginBottom: 48 }}>
+        <figure
+          onClick={handleClick}
+          style={{
+            position: "absolute",
+            top: `${position[0]}%`,
+            left: `${position[1]}%`,
+          }}
+        />
       </section>
       <footer>
-        <button>Jugar</button>
+        {status === "initial" && (
+          <button onClick={() => setStatus("playing")}>Jugar</button>
+        )}
+        {status === "playing" && (
+          <button onClick={() => setStatus("finished")}>Terminar</button>
+        )}
+        {status === "finished" && (
+          <button onClick={() => setStatus("initial")}>Reiniciar</button>
+        )}
       </footer>
     </main>
   );
